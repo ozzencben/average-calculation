0e70d2ce323bbcf9979924b4ba57e3c408ec4089
özenç <ozzencben@gmail.com>
2025-03-23 13:56:50 +0300

    Add files via upload

---
+++ practice/practice/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    public class averageCalculation
+    {
+
+        public void avrCalculate(string lastName, string name, decimal number1, decimal number2, decimal number3)
+        {
+
+            Console.WriteLine("Öğrenci ad : " + name);
+            Console.WriteLine("Öğrenci Soyad : " + lastName);
+
+            decimal result = (number1 + number2 + number3) / 3;
+
+            if (result >= 45)
+            {
+                Console.WriteLine("Ortalamanız : " + result + " - Tebrikler! Geçtiniz.");
+            }
+            else
+            {
+                Console.WriteLine("Ortalamanız : " + result + " - Maalesef, kaldınız.");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ practice/practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            averageCalculation averageCalculation = new averageCalculation();
+
+
+            Console.WriteLine("Öğrenci bilgilerini giriniz.");
+            
+            Console.Write("Öğrenci ad : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Öğrenci soyad : ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Öğrencinin  notlarını giriniz.");
+
+            Console.Write("Not 1 : ");
+            decimal number1 = decimal.Parse(Console.ReadLine());
+           
+            Console.Write("Not 2 : ");
+            decimal number2 = decimal.Parse(Console.ReadLine());
+            
+            Console.Write("Not 3 : ");
+            decimal number3 = decimal.Parse(Console.ReadLine());
+
+            averageCalculation.avrCalculate(name, lastName, number1, number2, number3);
+        }
+    }
+}
